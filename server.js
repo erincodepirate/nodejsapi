8c8f1ef35da37ac4eec6128f9ac714c9f732e288
Erin Clark <erincodepirate@gmail.com>
2022-08-02 18:37:05 -0700

    404 not found

--- server.js
+++ server.js
@@ -7,9 +7,19 @@ const todos = [
 ]
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('X-Powered-By', 'Node.js')
-    res.end(JSON.stringify({success: true, data: todos}));
+    res.statusCode = 404;
+    res.writeHead(404, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js'
+    })
+
+    res.end(JSON.stringify(
+        {
+            success: false, 
+            error: 'Not found',
+            data: null
+        }
+    ));
 });
 
 const PORT = 5000;
